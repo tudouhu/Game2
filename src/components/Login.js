1772cfdc09e65d218dbc96f388b1d58a38ea5be7
tudouhu <1838500094@qq.com>
2017-02-04 11:34:25 +0800

    login2

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,8 @@
  */
 import React from 'react';
 import Tools from '../common/Tools';
+import style from './Login.css';
+
 /**
  * 登录组件 继承React.Component
  */
@@ -14,7 +16,6 @@ class Login extends React.Component{
    */
   constructor(props){
     super(props);
-    this.state={type:'注册'};
   }
 
   /**
@@ -30,23 +31,35 @@ class Login extends React.Component{
    */
   render(){
       return(
-        <div ref='formDiv' style={{position:"relative",height:'100px'}}>
-          <div>
-            名字<input type="text" ref='name'/>
-          </div>
-          <div style={{position:"absolute",top:"20px"}}>
-            密码<input type="text" ref='password'/>
-          </div>
-          <button ref='tijiao' onClick={this.toggle} style={{position:"absolute",top:"50px"}}>
-            点击{this.state.type}
-          </button>
-        </div>
+       <div>
+         <div>
+           <img src="assets/img/icon.png"/>
+         </div>
+         <div ref='formDiv' className={style.bk}>
+           <h1>用户登录</h1>
+           <div className={style.nameDiv}>
+             名字<input type="text" ref='name'/>
+           </div>
+           <div className={style.passwordDiv}>
+             密码<input type="text" ref='password'/>
+           </div>
+           <div>
+             <button ref='tijiao' onClick={this.toggle} className={style.btn} >
+               登录
+             </button>
+             <button ref='tijiao' onClick={this.toggle1} className={style.btn} >
+               注册
+             </button>
+           </div>
+
+         </div>
+       </div>
       );
   }
 
 
   /**
-   * 提交
+   * 登录
    */
   toggle=()=>{
 
@@ -55,26 +68,19 @@ class Login extends React.Component{
       return;
     }
 
-    let d={name:this.refs.name.value,password:this.refs.password.value,type:this.state.type};
+    let d={name:this.refs.name.value,password:this.refs.password.value,type:'登陆'};
 
     Tools.ajax({data:d,url:'http://60.205.222.103:8000',mothed:'get',async:true,timeout:10000,
-    callback:(d)=>{
+      callback:(d)=>{
 
-      //{"data":"0"}  //已存在
-      //{"data":"1"}  // 注册成功
-      //{'data':'err'} //数据库错误
-      console.log(d);
+        //{"data":"0"}  //已存在
+        //{"data":"1"}  // 注册成功
+        //{'data':'err'} //数据库错误
+        console.log(d);
 
-      try{
-        let str=JSON.parse(d).data;
-        if(this.state.type=='注册'){
-          if(str=='0') alert('已注册');
-          else if(str=='1'){
-            alert('注册成功');
-            this.setState({type:'登陆'});
-          }
-        }
-        else{
+        try{
+          let str=JSON.parse(d).data;
+          console.log(str);
           if(str=='0') alert('用户名或者密码错误');
           else if(str =='1') {
             alert('登陆成功');
@@ -82,12 +88,47 @@ class Login extends React.Component{
             this.refs.tijiao.style.display = 'none';
           }
         }
+        catch(e){
 
+        }
       }
-      catch(e){
+    });
 
-      }
+  }
+
+  /**
+   * 注册
+   */
+  toggle1=()=>{
+
+    if(this.refs.name.value==''||this.refs.password.value==''){
+      alert('请输入完整的账号和密码');
+      return;
     }
+
+    let d={name:this.refs.name.value,password:this.refs.password.value,type:'注册'};
+
+    Tools.ajax({data:d,url:'http://60.205.222.103:8000',mothed:'get',async:true,timeout:10000,
+      callback:(d)=>{
+
+        //{"data":"0"}  //已存在
+        //{"data":"1"}  // 注册成功
+        //{'data':'err'} //数据库错误
+        console.log(d);
+
+        try{
+          let str=JSON.parse(d).data;
+          console.log(str);
+          if(str=='0') alert('已注册');
+          else if(str=='1'){
+            alert('注册成功');
+          }
+
+        }
+        catch(e){
+
+        }
+      }
     });
 
   }
