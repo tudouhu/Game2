44780c420656b4dc71c5064b8ee75bbefb842795
tudouhu <1838500094@qq.com>
2017-02-05 10:40:38 +0800

    icon

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,6 +2,7 @@
  * Created by tudouhu on 2017/2/2.
  */
 import React from 'react';
+import ReactDom from 'react-dom';
 import Tools from '../common/Tools';
 import style from './Login.css';
 
@@ -16,6 +17,7 @@ class Login extends React.Component{
    */
   constructor(props){
     super(props);
+    this.state = {type:'登陆'};
   }
 
   /**
@@ -24,6 +26,15 @@ class Login extends React.Component{
   componentDidMount(){
     //给姓名输入框添加输入事件
     this.refs.name.addEventListener("input",e=>{console.log(e.target.value)});
+
+    //循环插入图片
+    for(let i=1;i<=6;i++){
+      let str='<img src="assets/icon/icon_'+i+'.png"/>';
+      console.log(str);
+      // this.refs.iconDiv.appendChild(<img src={str}/>);
+      this.refs.iconDiv.innerHTML+=str;
+    }
+
   }
 
   /**
@@ -31,27 +42,30 @@ class Login extends React.Component{
    */
   render(){
       return(
-       <div>
-         <div>
+       <div className={style.bk}>
+         <div  className={style.heroIcon} onClick={this.onCilckImg}>
+           {/*<img src="assets/icon/icon_1.png" />*/}
            <img src="assets/img/icon.png"/>
          </div>
          <div ref='formDiv' className={style.bk}>
-           <h1>用户登录</h1>
+           <h1>用户{this.state.type}</h1>
            <div className={style.nameDiv}>
              名字<input type="text" ref='name'/>
            </div>
            <div className={style.passwordDiv}>
              密码<input type="text" ref='password'/>
            </div>
-           <div>
-             <button ref='tijiao' onClick={this.toggle} className={style.btn} >
-               登录
-             </button>
-             <button ref='tijiao' onClick={this.toggle1} className={style.btn} >
-               注册
-             </button>
+           <button ref='tijiao' onClick={this.toggle} className={style.btn} >
+             {this.state.type}
+           </button>
+           <div  className={style.sign}>
+             <u onClick={this.change}>用户注册</u>
            </div>
+         </div>
+         <div className={style.iconDiv}>
+           <div ref='iconDiv'>
 
+           </div>
          </div>
        </div>
       );
@@ -61,14 +75,14 @@ class Login extends React.Component{
   /**
    * 登录
    */
-  toggle=()=>{
+  toggle = e=>{
 
-    if(this.refs.name.value==''||this.refs.password.value==''){
-      alert('请输入完整的账号和密码');
+    if(this.refs.name.value==''||this.refs.password.value=='') {
+      alert('请输入完整的账户和密码');
       return;
     }
 
-    let d={name:this.refs.name.value,password:this.refs.password.value,type:'登陆'};
+    let d = {name:this.refs.name.value,password:this.refs.password.value,type:this.state.type};
 
     Tools.ajax({data:d,url:'http://60.205.222.103:8000',mothed:'get',async:true,timeout:10000,
       callback:(d)=>{
@@ -79,60 +93,54 @@ class Login extends React.Component{
         console.log(d);
 
         try{
-          let str=JSON.parse(d).data;
-          console.log(str);
-          if(str=='0') alert('用户名或者密码错误');
-          else if(str =='1') {
-            alert('登陆成功');
-            this.refs.formDiv.innerHTML = '欢迎你'+this.refs.name.value;
-            this.refs.tijiao.style.display = 'none';
+
+          var str = JSON.parse(d).data;
+          if(this.state.type == '注册'){
+            if(str=='0') alert('已存在');
+            else if(str =='1') {
+              alert('注册成功');
+              this.setState({type:'登陆'});
+            }
+
           }
+          else{
+            if(str=='0') alert('用户名或者密码错误');
+            else if(str =='1') {
+              alert('登陆成功');
+              this.refs.formDiv.innerHTML = '欢迎你'+this.refs.name.value;
+              this.refs.tijiao.style.display = 'none';
+            }
+          }
+
         }
         catch(e){
 
         }
-      }
-    });
+      }}
+    )
+
 
   }
 
   /**
-   * 注册
+   * 注册登陆切换
    */
-  toggle1=()=>{
-
-    if(this.refs.name.value==''||this.refs.password.value==''){
-      alert('请输入完整的账号和密码');
-      return;
+  change=(e)=>{
+    if(this.state.type=='登陆'){
+      e.target.innerHTML ='用户'+this.state.type;
+      this.setState({type:'注册'});
     }
-
-    let d={name:this.refs.name.value,password:this.refs.password.value,type:'注册'};
-
-    Tools.ajax({data:d,url:'http://60.205.222.103:8000',mothed:'get',async:true,timeout:10000,
-      callback:(d)=>{
-
-        //{"data":"0"}  //已存在
-        //{"data":"1"}  // 注册成功
-        //{'data':'err'} //数据库错误
-        console.log(d);
-
-        try{
-          let str=JSON.parse(d).data;
-          console.log(str);
-          if(str=='0') alert('已注册');
-          else if(str=='1'){
-            alert('注册成功');
-          }
-
-        }
-        catch(e){
-
-        }
-      }
-    });
+    else if(this.state.type=='注册') {
+      e.target.innerHTML ='用户'+this.state.type;
+      this.setState({type:'登陆'});
+    }
 
   }
 
+
+  onCilckImg=(e)=>{
+    console.log(e,e.target.src);
+  }
 
 
 }
